8be367096913832cef9632f7537a7a68c37c0e12
施胡埃 <seahore@whu.edu.cn>
2022-04-14 17:58:24 +0800

    Ch8 Bagging

--- MLTestgroundCSharp/Ch8.py
+++ MLTestgroundCSharp/Ch8.py
@@ -6,6 +6,8 @@ import numpy as np
 from sklearn import tree
 import graphviz
 
+rand = np.random.default_rng(int(time()))
+
 data = [
     [ 0.697, 0.460 ],
     [ 0.774, 0.376 ],
@@ -28,7 +30,7 @@ data = [
 
 target = [1, 1, 1, 1, 1, 1, 1, 1, -1, -1, -1, -1, -1, -1, -1, -1, -1]
 
-class ensemble_decision_tree:
+class boost_ensemble_tree:
     def __init__(self):
         self.__learners : list[tree.DecisionTreeClassifier] = []
         self.__weights : list[float] = []
@@ -45,17 +47,13 @@ class ensemble_decision_tree:
         return sum
 
 
-def ada_boost(data:list[list[float]], labels:list[float], epoch:int) -> ensemble_decision_tree:
+def ada_boost(data:list[list[float]], labels:list[float], epoch:int) -> boost_ensemble_tree:
     dist = [1 / len(data)] * len(data)
-    esm_tree = ensemble_decision_tree()
-    rand = np.random.default_rng(int(time()))
+    esm_tree = boost_ensemble_tree()
     for t in range(epoch):
         # 训练基分类器，得出其分类结果
         dtree = tree.DecisionTreeClassifier()
         dtree.fit(rand.choice(data, len(data), p=dist), labels)
-
-
-
         pred = dtree.predict(data)
         # 估计误差
         err = 0.0
@@ -64,8 +62,8 @@ def ada_boost(data:list[list[float]], labels:list[float], epoch:int) -> ensemble
                 err += 1 / len(pred)
         if err > 0.3:    # err > 0.5 抛弃的话，似乎不行
             continue
-        if err < 1E-10:
-            ultimate_tree = ensemble_decision_tree()
+        if err < 1E-10:  # err 极小权重近于正无穷，直接返回这个学习器
+            ultimate_tree = boost_ensemble_tree()
             ultimate_tree.add(dtree, 1.0)
             return ultimate_tree
         # 确定权重
@@ -83,21 +81,54 @@ def ada_boost(data:list[list[float]], labels:list[float], epoch:int) -> ensemble
 
 
 
+class bagging_ensemble_tree:
+    def __init__(self):
+        self.__learners : list[tree.DecisionTreeClassifier] = []
+    def add(self, learner:tree.DecisionTreeClassifier):
+        self.__learners.append(learner)
+    def evaluate(self, test_data:list[float]) -> float:
+        preds = []
+        for t in self.__learners:
+            preds.append(t.predict([test_data]))
+        return max(preds, key=preds.count)
+
+def bagging(data:list[list[float]], labels:list[float], epoch:int) -> bagging_ensemble_tree:
+    esm_tree = bagging_ensemble_tree()
+    for t in range(epoch):
+        # Bootstrap
+        train_indices = []
+        for i in range(len(data)):
+            train_indices.append(np.random.randint(0, len(data)))
+        test_indices = np.setdiff1d(range(len(data)), train_indices)
+        dtree = tree.DecisionTreeClassifier(max_depth=2)
+        train_data = []
+        train_labels = []
+        for i in train_indices:
+            train_data.append(data[i])
+            train_labels.append(labels[i])
+        dtree.fit(train_data, train_labels)
+        test_data = []
+        test_labels = []
+        for i in test_indices:
+            test_data.append(data[i])
+            test_labels.append(labels[i])
+        esm_tree.add(dtree)
+    return esm_tree
 
 
-esm_tree = ada_boost(data, target, 100)
-err = 0.0
+
+esm_tree = bagging(data, target, 50)
 for i in range(len(data)):
     print(f"Prediction {i}: {esm_tree.evaluate(data[i])}")
 
 
 '''
-        dot_data = tree.export_graphviz(dtree, out_file=None,
-                     feature_names=["Density", "Sweetness"],
-                     class_names=["Good", "Bad"],
-                     filled=True, rounded=True,
-                     special_characters=True,
-                     fontname="Fira Sans")
-        graph = graphviz.Source(dot_data)
-        graph.render("suika")
+dot_data = tree.export_graphviz(dtree, out_file=None,
+             feature_names=["Density", "Sweetness"],
+             class_names=["Good", "Bad"],
+             filled=True, rounded=True,
+             special_characters=True,
+             fontname="Fira Sans")
+graph = graphviz.Source(dot_data)
+graph.render("suika")
 '''
